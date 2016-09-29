b0901829ed004411a66c686d4640b286c03a11d2
Shad <shad@Shads-MacBook-Pro.local>
2016-09-29 22:06:18 +0100

    First component commit

---
+++ KeyboardDodgingView.js
@@ -0,0 +1,113 @@
+/**
+ * @providesModule KeyboardDodgingView
+ * https://github.com/shaddeen/react-native-keyboard-dodging-view
+ * @flow
+ */
+
+import React from 'react';
+
+import {
+  View,
+  Keyboard,
+  LayoutAnimation,
+  Platform
+} from 'react-native';
+
+class KeyboardDodgingView extends React.Component {
+  static propTypes = {
+    ...View.propTypes,
+    verticalOffset: React.PropTypes.number
+  };
+
+  static defaultProps = {
+    verticalOffset: 0
+  };
+
+  constructor () {
+    super();
+
+    this.state = {
+      top: 0
+    };
+  }
+
+  componentWillMount () {
+    if (Platform.OS == 'ios') {
+      this._subscriptions = [
+        Keyboard.addListener('keyboardWillChangeFrame', (event) => this.onKeyboardChange(event)),
+        Keyboard.addListener('keyboardWillShow', (event) => this.onKeyboardWillShow(event)),
+        Keyboard.addListener('keyboardWillHide', (event) => this.onKeyboardWillHide(event))
+      ];
+    }
+  }
+
+  componentWillUnmount () {
+    Platform.OS == 'ios' && this._subscriptions.forEach((subscription) => subscription.remove());
+  }
+
+  onKeyboardWillShow (event) {
+    this._keyboardVisible = true;
+    this.onKeyboardChange(event);
+  }
+
+  onKeyboardWillHide (event) {
+    this._keyboardVisible = false;
+    this._keyboardHeight = 0;
+    this.onKeyboardChange(event);
+  }
+
+  onKeyboardChange (event) {
+    let top = 0;
+
+    if (!event || !this._frame) {
+      this.setState({top});
+      return;
+    }
+
+    const {duration, easing, startCoordinates, endCoordinates} = event;
+    const viewHeight = this._frame.height;
+    const viewTopOffset = this._frame.y;
+
+    if (this._keyboardHeight === endCoordinates.height) {
+      return;
+    }
+
+    if (this._keyboardHeight !== endCoordinates.height || startCoordinates.screenY > endCoordinates.screenY) {
+      if (endCoordinates.screenY - viewHeight < viewTopOffset) {
+        top = endCoordinates.screenY - viewTopOffset - viewHeight - this.props.verticalOffset;
+      }
+    }
+
+    this.animateLayout(duration, easing);
+    this.setState({top});
+    if (this._keyboardVisible) {
+      this._keyboardHeight = endCoordinates.height;
+    }
+  }
+
+  animateLayout (duration = 250, easing = 'keyboard') {
+    LayoutAnimation.configureNext({
+      duration: duration,
+      update: {
+        duration: duration,
+        type: LayoutAnimation.Types[easing]
+      }
+    });
+  }
+
+  onLayout (event) {
+    if (event && event.nativeEvent.layout !== this._frame) {
+      this._frame = event.nativeEvent.layout;
+      this.props.onLayout && this.props.onLayout(event);
+    }
+  }
+
+  render () {
+    const {style, ...props} = this.props;
+    return (
+      <View style={[style, {marginTop: this.state.top}]} {...props} onLayout={(event) => this.onLayout(event)} />
+    );
+  }
+}
+
+module.exports = KeyboardDodgingView;
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # react-native-keyboard-dodging-view
+
+A react-native component for iOS that shifts the view up when the keyboard is presented. There are a few examples of this available including [KeyboardAvoidingView](https://facebook.github.io/react-native/releases/next/docs/keyboardavoidingview.html) but I've run into issues with this, particularly when using multiple fields. This solution aims to tackle this problem.
+
+![demo](https://github.com/shaddeen/react-native-keyboard-dodging-view/master/img/demo.gif)
+
+##Setup
+
+`npm install --save react-native-keyboard-dodging-view`
+
+##Props
+
+| Prop  | Type  |  |
+|---|---|---|
+| `verticalOffset`  | `number`  | (Optional) Additional vertical offset  |
+|---|---|---|
+
+##Usage
+
+Import:
+```
+import KeyboardDodgingView from 'KeyboardDodgingView`
+```
+Example:
+```
+class Example extends Component {
+  render() {
+    return (
+      <View style={{backgroundColor: '#dedede', flex: 1, paddingTop: 400}}>
+        <KeyboardDodgingView>
+          <TextInput returnKeyType="next" onSubmitEditing={() => this.refs.secondField.focus()} style={{height: 50, margin: 10, marginBottom: 0, backgroundColor: 'white'}} />
+          <TextInput ref="secondField" returnKeyType="go" style={{height: 50, margin: 10, backgroundColor: 'white'}} />
+        </KeyboardDodgingView>
+      </View>
+    );
+  }
+}
+```
+
+##Features
+ - Dynamically calculates the height required to shift contents up
+ - Accounts for changes in keyboard size
+ - Handles multiple textfield selection
+
+##Outstanding issues
+ - Slight jump when switching text fields via the keyboard return key on iOS 8
+ - Measurements are off when content is vertically aligned by a container view (with `justifyContent: 'center`)
