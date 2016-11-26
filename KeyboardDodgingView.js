29ac05de26ee1d05c7ff4ef0ddf79fb04fe173b1
Adam Wilson <adam@codegarden.co.uk>
2016-11-26 21:14:44 +0000

    WIP Fix for TextInputs at top of screen

--- KeyboardDodgingView.js
+++ KeyboardDodgingView.js
@@ -9,7 +9,9 @@ import React from 'react';
 import {
   View,
   Keyboard,
+  TextInput,
   LayoutAnimation,
+  UIManager,
   Platform
 } from 'react-native';
 
@@ -68,22 +70,74 @@ class KeyboardDodgingView extends React.Component {
     const viewHeight = this._frame.height;
     const viewTopOffset = this._frame.y;
 
-    if (this._keyboardHeight === endCoordinates.height) {
-      return;
-    }
+    /// Testing
+    const { State: TextInputState } = TextInput;
+    const currentlyFocusedField = TextInputState.currentlyFocusedField();
 
-    if (this._keyboardHeight !== endCoordinates.height || startCoordinates.screenY > endCoordinates.screenY) {
-      if (endCoordinates.screenY - viewHeight < viewTopOffset) {
-        top = endCoordinates.screenY - viewTopOffset - viewHeight - this.props.verticalOffset;
-      }
-    }
+    // if (currentlyFocusedField) {
+    //   UIManager.measure(currentlyFocusedField, (originX, originY, width, height, pageX, pageY) => {
+    //     console.log(originX, originY, width, height, pageX, pageY);
+    //     if (pageY < 0) {
+    //       console.log("Field was hidden!")
+    //       this.setState({top});
+    //       this._keyboardVisible = false;
+    //       this._keyboardHeight = 0;
+    //       return;
+    //     }
+    //   });
+    // }
+    if (currentlyFocusedField) {
+      UIManager.measure(currentlyFocusedField, (originX, originY, width, height, pageX, pageY) => {
+        console.log(originX, originY, width, height, pageX, pageY);
 
-    this.animateLayout(duration, easing);
-    this.setState({top});
-    if (this._keyboardVisible) {
-      this._keyboardHeight = endCoordinates.height;
+        console.log("keyboardHeight:" + this._keyboardHeight)
+        if (pageY < 250) { //this._keyboardHeight) {
+          this.setState({top});
+          this._keyboardVisible = false;
+          this._keyboardHeight = 0;
+          return;
+        }
+
+        if (this._keyboardHeight === endCoordinates.height) {
+          return;
+        }
+
+        if (this._keyboardHeight !== endCoordinates.height || startCoordinates.screenY > endCoordinates.screenY) {
+          if (endCoordinates.screenY - viewHeight < viewTopOffset) {
+            top = endCoordinates.screenY - viewTopOffset - viewHeight - this.props.verticalOffset;
+          }
+        }
+
+        this.animateLayout(duration, easing);
+        this.setState({top});
+        if (this._keyboardVisible) {
+          this._keyboardHeight = endCoordinates.height;
+        }
+      });
     }
+    else {
+      // TODO: put this in a method!!  
+      console.log("Measure not")
+        if (this._keyboardHeight === endCoordinates.height) {
+          return;
+        }
+
+        if (this._keyboardHeight !== endCoordinates.height || startCoordinates.screenY > endCoordinates.screenY) {
+          if (endCoordinates.screenY - viewHeight < viewTopOffset) {
+            top = endCoordinates.screenY - viewTopOffset - viewHeight - this.props.verticalOffset;
+          }
+        }
+
+        this.animateLayout(duration, easing);
+        this.setState({top});
+        if (this._keyboardVisible) {
+          this._keyboardHeight = endCoordinates.height;
+        }
+    }
+        /// 
+
   }
+
 
   animateLayout (duration = 250, easing = 'keyboard') {
     LayoutAnimation.configureNext({
@@ -93,7 +147,7 @@ class KeyboardDodgingView extends React.Component {
         type: LayoutAnimation.Types[easing]
       }
     });
-  }
+  }  
 
   onLayout (event) {
     if (event && event.nativeEvent.layout !== this._frame) {
